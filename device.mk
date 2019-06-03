e45d03e6a41afc3e64bb190efb44046d4776628b
mdeejay <mdjrussia@gmail.com>
2019-06-03 03:06:50 +0200

    Revert "violet: Build thermal hal from source"
    This reverts commit 708819b38915349e161ee09da3fab83cab516554.

--- device.mk
+++ device.mk
@@ -344,8 +344,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
+    android.hardware.thermal@1.0-service
 
 # Touchscreen
 PRODUCT_PACKAGES += \
